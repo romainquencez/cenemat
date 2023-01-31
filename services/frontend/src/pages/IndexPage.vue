c3cab3ca14fe05a0974e2d566e55c5e899694f00
romainquencez <contact@romainquencez.fr>
2023-01-31 21:02:51 +0100

    Améliore la page d'accueil

--- services/frontend/src/pages/IndexPage.vue
+++ services/frontend/src/pages/IndexPage.vue
@@ -1,46 +1,79 @@
 <template>
-  <q-page class="header text-white">
-    <h1 class="text-italic text-center no-margin">Bienvenue</h1>
-    <h2 class="text-center">
-      dans le <span class="text-uppercase">c</span>ercle d'<span class="text-uppercase">en</span>traide<br/>
-      et d'<span class="text-uppercase">e</span>change de <span class="text-uppercase">mat</span>ériel
-    </h2>
-
-  </q-page>
-
   <q-page>
-    <h3>
-      Une association d'agriculteurs, pourquoi faire ?
-    </h3>
+    <div class="header">
+      <div class="content q-py-sm">
+        <h1 class="text-italic text-center text-h3 q-mt-lg q-mb-sm no-padding">Bienvenue</h1>
+        <h2 class="text-center text-h5 q-mt-sm q-mb-lg">
+          dans le <span class="text-uppercase">c</span>ercle d'<span class="text-uppercase">en</span>traide<br/>
+          et d'<span class="text-uppercase">e</span>change de <span class="text-uppercase">mat</span>ériel
+        </h2>
+      </div>
+    </div>
 
-    <p>
-      <q-icon name="check" /> Réaliser ses travaux agricoles au bon moment dans de bonnes conditions.<br />
-      <q-icon name="check" /> Faire face en cas de panne de matériel.<br />
-      <q-icon name="check" /> Bénéficier de matériels spécialisés et performants sans avoir à investir.<br />
-      <q-icon name="check" /> Rentabiliser son matériel en travaillant pour d'autres en tant qu'offreur.<br />
-      <q-icon name="check" /> Diminuer ses charges de mécanisation.<br />
-      <q-icon name="check" /> Différer ou éviter un investissement trop lourd.<br />
-      <q-icon name="check" /> Organiser des chantiers efficaces et rapides.<br />
-      <q-icon name="check" /> Diminuer les pointes de travail.<br />
-      <q-icon name="check" /> Profiter de la disponibilité et de la compétence d'autres agriculteurs en travaillant ensemble.
-    </p>
+    <div class="q-px-md">
+      <h2 class="text-h4 q-mb-md">
+        Une association d'agriculteurs, pourquoi faire ?
+      </h2>
 
-    <hr />
+      <p>
+        <q-icon name="check" /> Réaliser ses travaux agricoles au bon moment dans de bonnes conditions.<br />
+        <q-icon name="check" /> Faire face en cas de panne de matériel.<br />
+        <q-icon name="check" /> Bénéficier de matériels spécialisés et performants sans avoir à investir.<br />
+        <q-icon name="check" /> Rentabiliser son matériel en travaillant pour d'autres en tant qu'offreur.<br />
+        <q-icon name="check" /> Diminuer ses charges de mécanisation.<br />
+        <q-icon name="check" /> Différer ou éviter un investissement trop lourd.<br />
+        <q-icon name="check" /> Organiser des chantiers efficaces et rapides.<br />
+        <q-icon name="check" /> Diminuer les pointes de travail.<br />
+        <q-icon name="check" /> Profiter de la disponibilité et de la compétence d'autres agriculteurs en travaillant ensemble.
+      </p>
 
-    <h3>
-      Rejoindre le CENEMAT
-    </h3>
+      <h2 class="text-h4 q-my-md">
+        Rejoindre le CENEMAT
+      </h2>
 
-    <p>
-      <q-icon name="phone" /> <a href="phone:+33686430167">06 86 43 01 67</a><br />
-      <q-icon name="email" /> <a href="mailto:association.cenemat@wanadoo.fr">association.cenemat@wanadoo.fr</a><br />
-      <q-icon name="house" /> <a href="https://www.google.com/maps/place/16+Chem.+de+Rougemont,+39100+Foucherans/" target="_blank">16, chemin de Rougemon 39100 FOUCHERANS</a>
-    </p>
+      <div class="row">
+        <div class="col-12 col-md-4 q-pa-sm">
+          <q-btn
+            stack
+            class="full-width"
+            color="primary"
+            label="06 86 43 01 67"
+            icon="phone"
+            href="tel:0686430167"
+          />
+        </div>
 
-    <p>
-      <span class="text-italic">Le CENEMAT est une association de loi 1901, il fonctionne grâce aux cotisations de ses adhérents.</span><br />
-      La cotisation est de <span class="text-weight-bold">130</span> euros.
-    </p>
+        <div class="col-12 col-md-4 q-pa-sm">
+          <q-btn
+            stack
+            class="full-width"
+            color="primary"
+            label="association.cenemat@wanadoo.fr"
+            icon="mail"
+            href="mailto:association.cenemat@wanadoo.fr"
+          />
+        </div>
+
+        <div class="col-12 col-md-4 q-pa-sm">
+          <q-btn
+            stack
+            class="full-width"
+            color="primary"
+            label="16 chemin de Rougemon 39100 FOUCHERANS"
+            icon="map"
+            target="_blank"
+            href="https://www.google.com/maps/place/16+Chem.+de+Rougemont,+39100+Foucherans/"
+          />
+        </div>
+      </div>
+
+      <p class="q-mt-lg text-italic">
+        Le CENEMAT est une association de loi 1901, il fonctionne grâce aux cotisations de ses adhérents.
+      </p>
+      <p>
+        La cotisation est de <span class="text-weight-bold">130</span> euros.
+      </p>
+    </div>
   </q-page>
 </template>
 
@@ -56,6 +89,12 @@ export default defineComponent({
 .header {
   background: url('assets/background.png') no-repeat;
   background-size: cover;
+
+  .content {
+    width: 100%;
+    height: 100%;
+    background-color: rgba(255, 255, 255, 0.5);
+  }
 }
 h1 {
   padding-top: 5rem;
