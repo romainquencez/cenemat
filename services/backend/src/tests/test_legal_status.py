94dacb7d0162232f7202d2000164554fd69be0a2
romainquencez <contact@romainquencez.fr>
2023-02-14 21:13:48 +0100

    Supprime le test des legal status

--- services/backend/src/tests/test_legal_status.py
+++
@@ -1,8 +0,0 @@
-def test_post_legal_status(test_app):
-    response = test_app.post(
-        "/legal-status/",
-        json={
-            "name": "SARL",
-        },
-    )
-    assert response.status_code == 201
