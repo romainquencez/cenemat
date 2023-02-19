8380f0a97a58b518d851b364a84a495d2f208830
romainquencez <contact@romainquencez.fr>
2023-02-19 22:25:17 +0100

    Revert "Front - Affiche les env Matomo"
    This reverts commit 4b09d209321e179f0ac35d3989037a67b8f806e2.

--- services/frontend/src/layouts/MainLayout.vue
+++ services/frontend/src/layouts/MainLayout.vue
@@ -137,14 +137,6 @@
             <q-item-section>
               {{ version }}
             </q-item-section>
-
-            <q-item-section>
-              Host : {{ matomoHost }}
-            </q-item-section>
-
-            <q-item-section>
-              Site ID : {{ matomoSiteId }}
-            </q-item-section>
           </q-item>
         </q-list>
       </q-scroll-area>
@@ -173,8 +165,6 @@ export default defineComponent({
       userStore,
       leftDrawerOpen,
       version,
-      matomoHost: process.env.MATOMO_HOST,
-      matomoSiteId: process.env.MATOMO_SITE_ID,
       toggleLeftDrawer () {
         leftDrawerOpen.value = !leftDrawerOpen.value
       },
